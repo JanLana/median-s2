1d795af72a0e907bee4116a80117e01bc17551b2
jaroslav.jezl.s <jaroslav.jezl.s@gyarab.cz>
2026-09-11 11:07:53 +0200

    dA

--- src/main/java/JaroslavJezl/NajdiMedian.java
+++ src/main/java/JaroslavJezl/NajdiMedian.java
@@ -1,27 +1,29 @@
 package JaroslavJezl;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class NajdiMedian {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Double> cisla = new ArrayList<>();
 
-        boolean konec = false;
-        int vstup = 1;
-
-      while (konec == false){
-            vstup = sc.nextInt();
-
-            if (vstup == 0){
-                konec = true;
-            }
-            else {
-                arr.add(vstup);
-            }
-
+        double a = sc.nextDouble();
+        while (a != 0) {
+            cisla.add(a);
+            a = sc.nextDouble();
         }
 
+        Collections.sort(cisla);
 
+        int stred = cisla.size() / 2;
+
+        if (cisla.size() % 2 == 0) {
+            System.out.println((cisla.get(stred - 1) + cisla.get(stred)) / 2);
+        } else {
+            System.out.println(cisla.get(stred));
+        }
     }
 }
