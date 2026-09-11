247be48d038b41bfc5c24f9017115e480dcb3ed2
ondrej.glatzel.s <ondrej.glatzel.s@gyarab.cz>
2026-09-11 11:04:47 +0200

    Muj Median

---
+++ src/main/java/Ondřej/Median.java
@@ -0,0 +1,38 @@
+package Ondřej;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+    public class Median {
+        static void main() {
+            Scanner sc = new Scanner(System.in);
+            List<Integer> arr = new ArrayList<Integer>();
+
+            while (sc.hasNextInt()) {
+                int cislo = sc.nextInt();
+                if (cislo == 0) {
+                    break;
+                }
+                arr.add(cislo);
+            }
+
+
+            if (arr.isEmpty()) {
+                return;
+            }
+
+            Collections.sort(arr);
+
+            int size = arr.size();
+            int median = arr.get(size / 2);
+
+
+
+            System.out.println("Medián je: " + median);
+
+            sc.close();
+        }
+    }
+
