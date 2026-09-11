910dba3545f005831c720097ba5f02d7fc7979b3
David <david.antonin.s@gyarab.cz>
2026-09-11 10:52:14 +0200

    jop

---
+++ src/main/java/David/medain.java
@@ -0,0 +1,28 @@
+package David; // <-- Zde upravte podle vašeho balíčku
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class medain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> cisla = new ArrayList<>();
+
+        double a = sc.nextDouble();
+        while (a != 0) {
+            cisla.add(a);
+            a = sc.nextDouble();
+        }
+
+        Collections.sort(cisla);
+
+        int stred = cisla.size() / 2;
+
+        if (cisla.size() % 2 == 0) {
+            System.out.println((cisla.get(stred - 1) + cisla.get(stred)) / 2);
+        } else {
+            System.out.println(cisla.get(stred));
+        }
+    }
+}
