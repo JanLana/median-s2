ec2fefb10b50d6a266bebb9f9217d49edb17521a
jaroslav.jezl.s <jaroslav.jezl.s@gyarab.cz>
2026-09-11 10:57:53 +0200

    d

---
+++ src/main/java/JaroslavJezl/NajdiMedian.java
@@ -0,0 +1,27 @@
+package JaroslavJezl;
+
+
+import java.util.Scanner;
+
+public class NajdiMedian {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        boolean konec = false;
+        int vstup = 1;
+
+      while (konec == false){
+            vstup = sc.nextInt();
+
+            if (vstup == 0){
+                konec = true;
+            }
+            else {
+                arr.add(vstup);
+            }
+
+        }
+
+
+    }
+}
