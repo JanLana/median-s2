f908f3196b8431b76c3e11d96da066c90a439833
OliverHyan <oliver.hyan.s@gyarab.cz>
2026-09-11 10:53:23 +0200

    nedodelany median

---
+++ src/main/java/hyan/Median.java
@@ -0,0 +1,31 @@
+package hyan;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Median {
+    public static void main() {
+        int daneCislo ;
+        int pocetCisel;
+
+        List<Integer> arr = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        pocetCisel = new ArrayList<>();
+        daneCislo = sc.nextInt();
+        while (daneCislo!=0) {
+            arr.add(daneCislo);
+            daneCislo = sc.nextInt();
+        }
+        if (pocetCisel % 2 =0) {
+
+
+
+        } else {
+
+
+
+        }
+    }
+
+}
